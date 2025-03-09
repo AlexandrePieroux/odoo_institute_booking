a2b5e6b116a2a60e37e89dd5828b0f3302908847
AlexandrePieroux <alex.pieroux@gmail.com>
2025-03-09 22:52:25 +0100

    init

---
+++ institute-booking/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Institute Booking System',
+    'category': 'Website/Website',
+    'sequence': 50,
+    'summary': 'Allow clients to book for services appointment',
+    'version': '1.0',
+    'depends': [
+        'website', 
+        'sale', 
+        'website_payment'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
